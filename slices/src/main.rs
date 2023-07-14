47d5873c3d85c1d80c77a8db2b290752ad9c07f2
cafedelmar99 <zukachakhnashvili@gmail.com>
2023-07-14 14:22:55 +0400

    slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let arr = [1,2,3,4];
-    let slice = &arr[1..3];
+    let slice = &arr[1 .. 3];
+    borrowing_slice(arr, slice);
+}
 
-    
+fn borrowing_slice(arr: [u8; 4], slice: &[u8]){
+    println!("{:?}", arr);
+    println!("{:?}", slice);
+    println!("length: {}", slice.len());
+    println!{"{} {}", slice[0], slice[1]};
 }
